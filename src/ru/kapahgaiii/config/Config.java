ef7afb23a82877878c5d3fa556174d7dbb657b50
KaPaHgaIII <sined_444@mail.ru>
2014-08-12 11:33:04 +0400

    added config from xml

--- src/ru/kapahgaiii/config/Config.java
+++ src/ru/kapahgaiii/config/Config.java
@@ -1,19 +1,35 @@
 package ru.kapahgaiii.config;
 
+import java.io.FileInputStream;
 import java.util.Properties;
 
 public class Config {
-    final public static int PORT = 4568;
-    final public static String HOST_IP = "178.62.30.124";
-    //final public static String HOST_IP = "localhost";
-    final public static String BINDING_NAME = "AccountService";
+    public static int PORT;
+    //final public static String HOST_IP = "178.62.30.124";
+    public static String HOST_IP;
+    public static String BINDING_NAME;
 
     final public static Properties SQLConnInfo = new Properties();
+    public static String JDBC_URL;
 
     static {
-        SQLConnInfo.put("characterEncoding", "UTF8");
-        SQLConnInfo.put("user", "root");
-        SQLConnInfo.put("password", "951159");
+        try {
+            Properties props = new Properties();
+            props.loadFromXML(new FileInputStream("config.xml"));
+
+            PORT = Integer.parseInt(props.getProperty("service.port"));
+            HOST_IP = props.getProperty("service.host_ip");
+            BINDING_NAME = props.getProperty("service.binding_name");
+
+            SQLConnInfo.put("characterEncoding", "UTF8");
+            SQLConnInfo.put("user", props.getProperty("jdbc.user"));
+            SQLConnInfo.put("password", props.getProperty("jdbc.pass"));
+
+            JDBC_URL = props.getProperty("jdbc.url");
+        } catch (Exception e) {
+            System.out.println("Error while reading config.xml");
+            System.err.println(e.toString());
+            System.exit(0);
+        }
     }
-    final public static String JDBC_URL = "jdbc:mysql://localhost/account_service";
 }
